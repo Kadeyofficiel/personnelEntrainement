66ed5c70cf482f1005488a30fa6d330e7c97e988
Kadeyofficiel <bakhtmohamed952@gmail.com>
2025-03-29 09:14:42 +0100

    Test unitaire de la classe gestion personnel

--- Personnel/src/testsUnitaires/testGestionPersonnel.java
+++ Personnel/src/testsUnitaires/testGestionPersonnel.java
@@ -1,5 +1,29 @@
 package testsUnitaires;
 
-public class testGestionPersonnel {
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import personnel.*;
+
+class testGestionPersonnel 
+{
+	GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
+	//TEST UNITAIRE DE GESTION PERSONNEL
+	@Test
+	void addLigue() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Flechettes");
+		assertEquals("Flechettes", ligue.getNom());
+	}
+
+	@Test
+	void addEmploye() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty", LocalDate.of(2023, 1, 1), LocalDate.of(2023, 12, 30)); 
+		assertEquals(employe, ligue.getEmployes().first());
+	}
 }
